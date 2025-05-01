a845595ce2b46dae99f1051402bb08876f6270cf
Akash Singh <akash6998s@gmail.com>
2025-05-01 14:19:44 +0530

    application code

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,14 +1,17 @@
 // src/serviceWorkerRegistration.js
+
+// This function registers the service worker
 export function register() {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
+        // Register service worker in the public folder
         navigator.serviceWorker
-          .register('/service-worker.js')
-          .then(registration => {
-            console.log('ServiceWorker registered: ', registration);
+          .register('/service-worker.js') // This should point to the service-worker.js in the public folder
+          .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
           })
-          .catch(registrationError => {
-            console.log('ServiceWorker registration failed: ', registrationError);
+          .catch((error) => {
+            console.error('Service Worker registration failed:', error);
           });
       });
     }
